020064ec3e073ec0ef2967461792d57744956595
shimo <shimouniverse0503@gmail.com>
2025-05-28 01:50:45 +0900

    データベースのmigrate

--- backend/src/app/Models/User.php
+++ backend/src/app/Models/User.php
@@ -2,47 +2,33 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    // パスワードなど自動で隠す（シリアライズ時に除外）
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    public function customers() {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function orders() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function products() {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function sales() {
+        return $this->belongsTo(Sale::class);
     }
 }

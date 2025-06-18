9f02784182aee293f8b5d0c6bc7dcbfcfbcfdb74
shimo0503 <shimouniverse0503@gmail.com>
2025-06-18 17:32:38 +0900

    モデルの修正

--- backend/src/app/Models/Customer.php
+++ backend/src/app/Models/Customer.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    protected $fillable = [
+        'table_number',
+        'price',
+        'user_id',
+    ];
     public function orders() {
         return $this->belongsTo(Order::class);
     }
